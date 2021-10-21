73b5a167354fb9192b8ee55fcb8b2ea2c16d0722
Indigo <iredward27@gmail.com>
2021-10-21 12:31:05 -0400

    undid previous commits due to large file - will fix later

--- scripts/exploring_files.R
+++ scripts/exploring_files.R
@@ -1,8 +1,14 @@
+# next steps: 
+# fix the fkn raster
+# make sure the CC buffers I pulled are actually good to use
+
 library(tidyverse)
 
 library(ggplot2)
 
 library(sf)
+
+library(rgdal)
 
 sites <- read_csv('data/raw/2021-06-22_Site.csv') %>% 
   filter(
@@ -10,6 +16,9 @@ sites <- read_csv('data/raw/2021-06-22_Site.csv') %>%
     Latitude < 50)
 
 us_map <- st_read('data/geographic/states.shp')
+
+nc <- us_map %>% 
+  filter(NAME == 'North Carolina')
 
 sites_sf <- 
   sites %>% 
@@ -21,3 +30,27 @@ ggplot(us_map) +
   geom_sf() +
   geom_sf(data = sites_sf) +
   theme_void()
+
+impervious <- raster::raster('data/geographic/nlcd_2019_impervious.img')
+
+raster::writeRaster(impervious, 'ncld_impervious_2019.tif')
+
+raster::extract(
+  impervious,
+  sites_sf %>% st_transform(crs = raster::crs(impervious)))
+
+cc_buffs <- st_read('data/geographic/catcount_site_buffers.shp') %>% 
+  st_transform(crs = st_crs(us_map))
+
+ggplot(nc) +
+  geom_sf() +
+  geom_sf(data = st_crop(cc_buffs, nc))
+
+mean_impervious <- 
+  raster::extract(
+  impervious,
+  cc_buffs %>% st_transform(crs = raster::crs(impervious)),
+  fun = mean,
+  na.rm = T,
+  sp = T) %>% 
+  as_tibble()
